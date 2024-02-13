3a653f8fa7bde6d0e80c9995f00c467b99b22b01
valmenu59 <valentin.menu@outlook.com>
2024-02-13 10:32:04 +0100

    Début projet fonctionnel

---
+++ app/src/main/java/com/example/mini_projet_notes/Main.java
@@ -0,0 +1,12 @@
+package com.example.mini_projet_notes;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
